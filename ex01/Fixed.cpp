ce7880d273a16c3647b695a5d65c035f7467e944
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-08-22 15:58:42 +0200

    toFloat and toInt and ostream

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,98 @@
+#include "Fixed.hpp"
+
+//	CONSTRUCTORS | DESTRUCTOR
+//	const int _nFracBits = 8;
+
+Fixed::Fixed():_integer(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+/**
+ * custom constructor converting int to fixed-point value. frac bits = 8;
+ * 
+ * @param number 
+ */
+Fixed::Fixed(int const i_number)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_integer = i_number * (1 << _nFracBits);
+	return ;
+}
+/**
+ * custom constructor converting float to fixed point value to store in int container.
+ * 
+ * @param number 
+ */
+Fixed::Fixed(float const f_number)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_integer = roundf(f_number * (1 << _nFracBits));
+}
+
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * @param obj obj reference to object instance given Fixed const& obj
+ */
+Fixed::Fixed(Fixed const& obj){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = obj; /* shallow vs deep copy? Instance = instance is which?
+	Better practice is to use initialisation list.? getter fkt in init list?
+	- this pointer cant be used in init list?
+	- this needs to be dereferenced to refer to instance itself and assigning that to ref obj.*/
+	return ;
+}
+
+// assignment operator overload => changes function of = variable when used in association with this class.
+Fixed& Fixed::operator=(const Fixed &obj)
+{
+	std::cout << "Copy assignment constructor called" << std::endl;
+	//this->_integer = obj.getRawBits();
+	if (this != &obj)
+		this->setRawBits(obj.getRawBits());
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+//	MEMBER FUNCTIONS
+
+int Fixed::getRawBits(void)const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_integer);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_integer = raw;
+	std::cout << "Setter called" << std::endl;
+	return ;
+}
+
+int Fixed::toInt(void)const
+{
+	int i;
+	i = this->_integer / (1 << _nFracBits);
+	return i;
+}
+
+//need to typecast int into float, so that its stored as float. One typecast is enough..?
+float Fixed::toFloat(void)const
+{
+	float f;
+	f = (float) this->_integer / (float)(1 << _nFracBits);
+	return f;
+
+}
+
+std::ostream& operator <<(std::ostream& o, Fixed const& fp)
+{
+	o << fp.toFloat();
+	return o;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+
+		Fixed(void);
+		Fixed(int const i_number);
+		Fixed(float const f_number);
+		Fixed(Fixed const& obj);
+		Fixed& operator=(Fixed const& obj);
+		~Fixed();
+
+		float toFloat(void) const;
+		int toInt(void) const;
+		int getRawBits(void)const; //returns raw value fixed-point value
+		void setRawBits(int const raw);
+
+	private:
+
+		int _integer;
+		static const int _nFracBits= 8; /* can I assigned it here or better in constructor? (Prob cant be in constr 
+		because its //static data member (non member attribute according to video which is wrong?)) */
+};
+
+std::ostream& operator<<(std::ostream& o, Fixed const& fp);
+
+#endif
+
+//fixed-point number: <8.3> 8 bits, 3 fractional. signed bits if negative -> 2's complement 
+
+//e.g 7.5 in 8 bit binary rep 0111 . 1000 -> 2 complement = 1000 . 1000
